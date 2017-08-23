24542268566cf4fb6b5a8d8618b07f87e340d912
brendanater <brendanater@gmail.com>
2017-08-23 16:47:25 -0600

    Initial commit

---
+++ SimplifiedCoder/SimplifiedCoder/SimplifiedCoder.h
@@ -0,0 +1,19 @@
+//
+//  SimplifiedCoder.h
+//  SimplifiedCoder
+//
+//  Created by Brendan Henderson on 8/23/17.
+//  Copyright © 2017 OKAY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplifiedCoder.
+FOUNDATION_EXPORT double SimplifiedCoderVersionNumber;
+
+//! Project version string for SimplifiedCoder.
+FOUNDATION_EXPORT const unsigned char SimplifiedCoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplifiedCoder/PublicHeader.h>
+
+
