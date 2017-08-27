7849acc824f7dc438446f88af92301154d144b7b
Bryan Henderson <bhenderson@izeni.com>
2017-08-27 09:18:48 -0600

    working on Decoder

---
+++ SimplifiedCoder/SimplifiedCoder/Decoder.swift
@@ -0,0 +1,15 @@
+//
+//  Decoder.swift
+//  SimplifiedCoder
+//
+//  Created by Christopher Bryan Henderson on 8/27/17.
+//  Copyright © 2017 OKAY. All rights reserved.
+//
+
+import Foundation
+
+protocol TopLevelDecoder {
+    
+    func decode<T: Decodable>(_: T.Type, from data: Data) throws -> T
+    func decode<T: Decodable>(_: T.Type, from value: Any) throws -> T
+}
