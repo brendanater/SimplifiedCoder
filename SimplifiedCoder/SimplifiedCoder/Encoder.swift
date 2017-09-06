6e851a4dbb2e30a84dc758d733c2d2abe0ba116a
brendanater <brendanater@gmail.com>
2017-09-06 16:32:34 -0600

    added all copyright permissions

--- SimplifiedCoder/SimplifiedCoder/Encoder.swift
+++ SimplifiedCoder/SimplifiedCoder/Encoder.swift
@@ -5,17 +5,32 @@
 //  Created by Brendan Henderson on 8/23/17.
 //  Copyright © 2017 OKAY. All rights reserved.
 //
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 import Foundation
 
-/// a TopLevelEncoder calls an encoder and verifies the encoded data before serializing
-/// this is the API that the user calls
-/// a TopLevelEncoder is not an encoder.
+/// the encoder that the user calls to abstract away complexity
 protocol TopLevelEncoder {
     
     func encode(_ value: Encodable) throws -> Data
-    
-    func encode(asValue value: Encodable) throws -> Any
 }
 
 // MARK: - EncoderBase
